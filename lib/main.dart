11404a441ad5571be59f0d98eedaef2333d83164
Hazrinbabakr <hazrin.babakr@gmail.com>
2022-09-12 13:30:14 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
         //accentColor: Colors.white
+        //eeeee
       ),
       home: Test()
     );
